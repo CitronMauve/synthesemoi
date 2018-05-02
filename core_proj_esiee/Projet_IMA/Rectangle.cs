5b3da1b5d4c30c74a1fc34f976cccdfdb541eb98
Huy-Hai Vincent NGUYEN <nhhvincent@gmail.com>
2018-05-02 11:05:08 +0200

    Method Draw in Rectangle class

--- core_proj_esiee/Projet_IMA/Rectangle.cs
+++ core_proj_esiee/Projet_IMA/Rectangle.cs
@@ -8,12 +8,18 @@ namespace Projet_IMA
         private V3 a;
         private V3 b;
         private V3 c;
+        private V3 normale;
 
         public Rectangle(V3 a, V3 b, V3 c, Texture texture) : base(texture)
         {
           this.a = a;
           this.b = b;
           this.c = c;
+
+          V3 vecteurAB = this.b - this.a;
+          V3 vecteurAC = this.c - this.a;
+          this.normale = vecteurAB ^ vecteurAC;
+          this.normale.Normalize();
         }
 
         public Rectangle(V3 a, V3 b, V3 c, Couleur couleur) : base(couleur)
@@ -21,9 +27,14 @@ namespace Projet_IMA
           this.a = a;
           this.b = b;
           this.c = c;
+
+          V3 vecteurAB = this.b - this.a;
+          V3 vecteurAC = this.c - this.a;
+          this.normale = vecteurAB ^ vecteurAC;
+          this.normale.Normalize();
         }
 
-        public V3 Calculer(float u, float v)
+        public override V3 Calculer(float u, float v)
         {
           V3 vecteurAB = this.b - this.a;
           V3 vecteurAC = this.c - this.a;
@@ -33,13 +44,39 @@ namespace Projet_IMA
         public override void Draw(V3 camera, int[,] zbuffer, List<Lampe> lampes)
         {
             float step = 0.01f;
-            for (float u = 0; u <= 2 * Math.PI; u += step)
+            for (float u = 0; u <= 1; u += step)
             {
-              for (float v = -(float) Math.PI / 2; v <= (float) Math.PI / 2; v += step)
+              for (float v = 0; v <= 1; v += step)
               {
+                V3 currentPoint;
+                currentPoint = Calculer(u, v);
+
+                int x = (int) (currentPoint.x);
+                int y = (int) (currentPoint.y);
+                int z = (int) (currentPoint.z);
+
+                if (y < zbuffer[z, x])
+                {
+                  Couleur couleurAffichee;
+                  V3 bumpNormale;
+
+                  if (this.texture != null)
+                  {
+                    this.couleur = this.texture.LireCouleur(u, v);
+
+                    bumpNormale = BumpNormale(u, v);
+                    bumpNormale.Normalize();
+                    this.normale = bumpNormale;
+                  }
+
+                  couleurAffichee = LampesEffectsOnCouleur(lampes, this.couleur, camera);
+
+                  BitmapEcran.DrawPixel(x, z, couleurAffichee);
+
+                  zbuffer[z, x] = y;
+                }
               }
             }
         }
-        
     }
 }
