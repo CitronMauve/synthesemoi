195dfa07b5d4cfcab75e665e3a1429df97686fa4
Huy-Hai Vincent NGUYEN <nhhvincent@gmail.com>
2018-05-02 11:04:25 +0200

    Moved some methods of Sphere to Objet

--- core_proj_esiee/Projet_IMA/Objet.cs
+++ core_proj_esiee/Projet_IMA/Objet.cs
@@ -23,6 +23,44 @@ namespace Projet_IMA
             this.bump = bump;
         }
 
+        public V3 CalculerDeriveeU(float u, float v)
+        {
+            return new V3(
+                (float) (Math.Cos(v) * -Math.Sin(u)),
+                (float) (Math.Cos(v) * Math.Cos(u)),
+                0
+            );
+        }
+
+        public V3 CalculerDeriveeV(float u, float v)
+        {
+            return new V3(
+                (float)(-Math.Sin(v) * Math.Cos(u)),
+                (float)(-Math.Sin(v) * Math.Sin(u)),
+                (float) Math.Cos(v)
+            );
+        }
+
+        public V3 BumpNormale(V3 normale, float u, float v)
+        {
+          float k = 2f;
+          this.texture.Bump(u, v, out float dhdu, out float dhdv);
+
+          return normale + k * ( (CalculerDeriveeU(u, v) ^ (dhdv * normale)) + ((dhdu * normale) ^ CalculerDeriveeV(u, v)) );
+        }
+
+        public Couleur LampesEffectsOnCouleur(List<Lampe> lampes, Couleur couleur, V3 camera)
+        {
+          Couleur couleurAffichee;
+          couleurAffichee = new Couleur();
+          foreach (Lampe lampe in lampes)
+          {
+              couleurAffichee += lampe.allEffects(couleur, normale, camera);
+          }
+          return couleurAffichee;
+        }
+
+        abstract public void Calculer(float u, float v);
         abstract public void Draw(V3 camera, int[,] zbuffer, List<Lampe> lampes);
     }
 }
